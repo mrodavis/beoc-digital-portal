f1ea63093ff41360cef44e531ad06ff80b4f366b
mrodavis <orville.davis@scrapit.io>
2026-03-08 22:54:54 -0400

    created lesson 1 in powerpoint module 1

--- lib/content/ms-office/powerpoint/module1Lessons.ts
+++ lib/content/ms-office/powerpoint/module1Lessons.ts
@@ -6,63 +6,95 @@ export const powerpointModule1Lessons: Lesson[] = [
   slug: "getting-started-with-powerpoint",
   title: "Getting Started with PowerPoint",
   description:
-    "Learn your way around the PowerPoint interface including the Ribbon, Backstage view, Quick Access Toolbar, slide pane, and presentation views.",
-  duration: "10 min",
-  videoUrl: "https://www.youtube.com/embed/FyP5f0pR4nA",
+    "Learn your way around the PowerPoint interface including the Ribbon, Quick Access Toolbar, slide views, zoom controls, and Backstage view.",
+  duration: "12 min",
+  videoUrl: "https://www.youtube.com/embed/k6pg4nZS6fA",
 
   practiceFiles: [],
 
   sections: [
 
     {
-      heading: "The PowerPoint Interface",
+      heading: "PowerPoint Start Screen",
       blocks: [
         {
           type: "paragraph",
           text:
-            "When you open PowerPoint, you’ll see a presentation made up of slides. The interface includes the slide canvas, slide thumbnails, and the Ribbon where most commands live.",
+            "When you open PowerPoint, the Start Screen appears. From here you can create a new presentation, choose from templates, or open recent files.",
         },
         {
+          type: "paragraph",
+          text:
+            "Select Blank Presentation to open the PowerPoint editing environment and begin building your slides.",
+        },
+                {
           type: "image",
-          src: "/images/powerpoint/module-1/interface/powerpoint_interface.png",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_interface.png",
           alt: "PowerPoint interface overview",
         },
       ],
     },
 
     {
-      heading: "Understanding the Ribbon",
+      heading: "Understanding the PowerPoint Interface",
       blocks: [
         {
           type: "paragraph",
           text:
-            "The Ribbon organizes PowerPoint commands into tabs such as Home, Insert, Design, Transitions, Animations, Slide Show, Review, and View.",
-        },
-        {
-          type: "image",
-          src: "/images/powerpoint/module-1/interface/powerpoint_ribbon_tabs.png",
-          alt: "PowerPoint ribbon tabs",
+            "Once a presentation opens, you will see the PowerPoint interface. The main workspace includes the Ribbon, slide navigation pane, slide editing area, and status bar.",
         },
         {
           type: "paragraph",
           text:
-            "Each tab contains command groups with related tools. For example, the Home tab contains Clipboard, Slides, Font, and Paragraph groups.",
+            "The slide pane is where you build your presentation, while the navigation pane on the left allows you to organize and switch between slides.",
+        },
+
+      ],
+    },
+
+    {
+      heading: "The Ribbon",
+      blocks: [
+        {
+          type: "paragraph",
+          text:
+            "The Ribbon contains the commands you use to create and edit presentations. It is organized into tabs such as Home, Insert, Design, Transitions, Animations, Slide Show, Review, and View.",
+        },
+        {
+          type: "paragraph",
+          text:
+            "Each tab contains groups of related commands. For example, the Font group contains tools for formatting text.",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_ribbon.png",
+          alt: "PowerPoint ribbon tabs",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_small_arrow.png",
+          alt: "Ribbon dialog launcher",
         },
       ],
     },
 
     {
-      heading: "Backstage View",
+      heading: "Showing and Hiding the Ribbon",
       blocks: [
         {
           type: "paragraph",
           text:
-            "Click the File tab to open Backstage view. This is where you manage files including opening presentations, saving files, printing slides, and sharing presentations.",
+            "You can control how much of the Ribbon is visible using the Ribbon Display Options in the top-right corner of PowerPoint.",
+        },
+        {
+          type: "paragraph",
+          text:
+            "Options include Auto-hide Ribbon, Show Tabs, and Show Tabs and Commands.",
         },
         {
           type: "image",
-          src: "/images/powerpoint/module-1/interface/powerpoint_backstage_view.png",
-          alt: "PowerPoint backstage view",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_ribbon_dropdown.png",
+          alt: "Ribbon display options",
         },
       ],
     },
@@ -73,38 +105,137 @@ export const powerpointModule1Lessons: Lesson[] = [
         {
           type: "paragraph",
           text:
-            "The Quick Access Toolbar appears in the upper-left corner and provides fast access to commonly used commands such as Save, Undo, and Redo.",
+            "The Quick Access Toolbar is located above the Ribbon and provides quick access to commonly used commands such as Save, Undo, Redo, and Start From Beginning.",
         },
         {
           type: "paragraph",
           text:
-            "You can customize the toolbar by adding commands you use frequently.",
+            "You can customize the Quick Access Toolbar by clicking the drop-down arrow and selecting additional commands.",
         },
         {
           type: "image",
-          src: "/images/powerpoint/module-1/interface/powerpoint_quick_access_toolbar.png",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_quick_access.png",
           alt: "Quick access toolbar",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_quick_print_setup.png",
+          alt: "Quick access customization menu",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_quick_print.png",
+          alt: "Quick print added to quick access toolbar",
         },
       ],
     },
 
     {
-      heading: "Slide Pane & Slide Workspace",
+      heading: "Using the Tell Me Box",
       blocks: [
         {
           type: "paragraph",
           text:
-            "The slide thumbnails appear on the left side of the screen. This pane allows you to navigate between slides and reorder them easily.",
+            "The Tell Me box works like a search tool inside PowerPoint. You can type the name of a command or feature and PowerPoint will help you find it instantly.",
         },
         {
           type: "paragraph",
           text:
-            "The large central area is the slide workspace where you edit the contents of your slide.",
+            "This is especially useful when you are learning PowerPoint and do not know where certain tools are located.",
         },
         {
           type: "image",
-          src: "/images/powerpoint/module-1/interface/powerpoint_slide_workspace.png",
-          alt: "Slide editing workspace",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_shapes.png",
+          alt: "Using the Tell Me box to search commands",
+        },
+      ],
+    },
+
+    {
+      heading: "Ruler, Guides, and Gridlines",
+      blocks: [
+        {
+          type: "paragraph",
+          text:
+            "PowerPoint includes layout tools to help align objects on your slides. These include the ruler, guides, and gridlines.",
+        },
+        {
+          type: "paragraph",
+          text:
+            "You can enable these tools from the View tab in the Ribbon under the Show group.",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_view_tools.png",
+          alt: "View tab ruler gridlines guides",
+        },
+      ],
+    },
+
+    {
+      heading: "Slide Views",
+      blocks: [
+        {
+          type: "paragraph",
+          text:
+            "PowerPoint includes multiple slide views that change how your presentation is displayed. These include Normal view, Slide Sorter view, Reading view, and Slide Show view.",
+        },
+        {
+          type: "paragraph",
+          text:
+            "You can switch between these views using the view buttons in the bottom-right corner of the PowerPoint window.",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_slide_view_command.png",
+          alt: "Slide view buttons",
+        },
+      ],
+    },
+
+    {
+      heading: "Zoom Controls",
+      blocks: [
+        {
+          type: "paragraph",
+          text:
+            "The Zoom slider located in the bottom-right corner allows you to zoom in or out of your slide.",
+        },
+        {
+          type: "paragraph",
+          text:
+            "This helps you adjust your workspace when working on detailed slide content.",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_zoom_command.png",
+          alt: "Zoom slider control",
+        },
+      ],
+    },
+
+    {
+      heading: "Backstage View",
+      blocks: [
+        {
+          type: "paragraph",
+          text:
+            "Backstage view is accessed by clicking the File tab. It contains options for saving, printing, sharing, exporting, and managing your presentation.",
+        },
+        {
+          type: "paragraph",
+          text:
+            "This area also shows presentation information such as file size, slide count, and document properties.",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_file_tab.png",
+          alt: "File tab backstage button",
+        },
+        {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_backstage_view.png",
+          alt: "PowerPoint backstage view",
         },
       ],
     },
@@ -120,14 +251,23 @@ export const powerpointModule1Lessons: Lesson[] = [
       type: "list",
       ordered: true,
       items: [
-        "Identify the slide thumbnails pane.",
-        "Locate the Ribbon tabs.",
-        "Open Backstage view and return to the presentation.",
-        "Customize the Quick Access Toolbar.",
-        "Switch between two slides in the slide pane.",
+        "Open PowerPoint and create a blank presentation.",
+        "Change the Ribbon Display Options to Show Tabs.",
+        "Add New, Quick Print, and Spelling to the Quick Access Toolbar.",
+        "Use the Tell Me box to search for Shapes.",
+        "Insert a shape onto the slide.",
+        "Enable the Ruler from the View tab.",
+        "Zoom the presentation to 120%.",
+        "Return the Ribbon display to Show Tabs and Commands.",
       ],
     },
+    {
+          type: "image",
+          src: "/images/powerpoint/module-1/getting-started/powerpoint_final_result.png",
+          alt: "PowerPoint backstage view",
+        },
   ],
+
 },
 
 {
